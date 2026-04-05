ff59a189dd2caa9556b7b0b803d29ee8d77bbe3f
Thajeepan-Rathiharan <thajee2002@gmail.com>
2026-04-05 17:40:45 +0100

    Updated CSS.

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Login</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="Assets/Bootstrap/CSS/bootstrap/css">
     <!-- My CSS stylesheet -->
     <link rel="stylesheet" href="Assets/MyCSS/style.css">
 </head>

e16d9762f223ce9db6cbabc0019965cb69b3c6b8
Thajeepan-Rathiharan <thajee2002@gmail.com>
2026-04-05 17:29:48 +0100

    Updates.

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Login</title>
-    <link rel="stylesheet" href="CSS/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <!-- CSS stylesheet -->
+    <link rel="stylesheet" href="Assets/MyCSS/style.css">
 </head>
 
 <?php
@@ -53,6 +57,10 @@
   <?php
   } 
 ?>
+
+<!-- Bootstrap JS -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
 
 </body>
 </html>

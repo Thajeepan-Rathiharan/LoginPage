c09ba249163944a45ba41d27743a5907d47624ec
Thajeepan-Rathiharan <thajee2002@gmail.com>
2026-04-05 19:22:18 +0100

    Logout function added.

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     if(!isset($_SESSION['login'])){
-        header('Location:../login/'); die();
+        header('Location:login.php'); die();
     } //If the user isn’t logged in, sends them back to the login page and stops everything else.
